d861fcc9c751e0935681de423462b06f31904087
basharat <basharatyousufi786@gmail.com>
2023-07-31 12:25:24 +0500

    my ne assignment

---
+++ assignment#13/execise#1.py
@@ -0,0 +1,15 @@
+#Exercise 1: Default Parameter Value
+#1- Create a Python function named print_message that takes a parameter message and a parameter
+# repeat_count, where repeat_count has a default value of 1.
+#2- The function should print the message the number of times specified by repeat_count.
+#3- Prompt the user to enter a message and a repeat count (optional).
+#4- Call the print_message function with the user-provided values or using the default .
+
+
+
+def print_message(message, repeat_count=1):
+  
+    print(message*repeat_count) 
+
+print_message("hello world",)
+print_message("hello world",5)
